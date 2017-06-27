575d2ee9c8b7f94b830c27f215093c3847a74ddc
Shawn McCabe <smccabe@acromediainc.com>
2017-06-27 16:05:20 -0700

    Adjust id file to be in generated folder

--- src/Id.php
+++ src/Id.php
@@ -7,15 +7,15 @@ class Id
 
     public static function getID()
     {
-        if (file_exists('id')) {
+        if (file_exists('generated/id')) {
             $id = (int) file_get_contents('id');
             $id++;
 
-            file_put_contents('id', $id);
+            file_put_contents('generated/id', $id);
 
             return $id;
         } else {
-            file_put_contents('id', '1');
+            file_put_contents('generated/id', '1');
             return 1;
         }
     }
