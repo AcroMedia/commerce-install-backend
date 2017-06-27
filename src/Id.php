d621fc2e3a7e888a6e1b7caa65bf132bb7c3215b
Shawn McCabe <smccabe@acromediainc.com>
2017-06-27 16:20:18 -0700

    fix incorrect path for id

--- src/Id.php
+++ src/Id.php
@@ -7,15 +7,16 @@ class Id
 
     public static function getID()
     {
-        if (file_exists('generated/id')) {
-            $id = (int) file_get_contents('id');
+        $filename = 'generated/id';
+        if (file_exists($filename)) {
+            $id = (int) file_get_contents($filename);
             $id++;
 
-            file_put_contents('generated/id', $id);
+            file_put_contents($filename, $id);
 
             return $id;
         } else {
-            file_put_contents('generated/id', '1');
+            file_put_contents($filename, '1');
             return 1;
         }
     }
