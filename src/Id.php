3016737f3cf23c1619154c2a294004d27176fb92
Jace Bennest <jbennest@acromediainc.com>
2017-08-18 15:13:17 -0700

    wip

--- src/Id.php
+++ src/Id.php
@@ -2,22 +2,21 @@
 
 namespace Kickstart;
 
-class Id
-{
+class Id {
 
-    public static function getID()
-    {
-        $filename = 'generated/id';
-        if (file_exists($filename)) {
-            $id = (int) file_get_contents($filename);
-            $id++;
+  public static function getID() {
+    $filename = 'generated/id';
+    if (file_exists($filename)) {
+      $id = (int) file_get_contents($filename);
+      $id++;
 
-            file_put_contents($filename, $id);
+      file_put_contents($filename, $id);
 
-            return $id;
-        } else {
-            file_put_contents($filename, '1');
-            return 1;
-        }
+      return $id;
     }
+    else {
+      file_put_contents($filename, '1');
+      return 1;
+    }
+  }
 }
