66bcb42ec5cfb907d6024271eff9375ccf7f9a05
Jace Bennest <jacebennest87@gmail.com>
2017-08-18 23:02:00 -0700

    start lightning class

--- src/Lightning.php
+++ src/Lightning.php
@@ -7,36 +7,32 @@ class Lightning extends Base {
   public function __construct() {
     parent::__construct('Kickstart', 'Automatically Generated Commerce Kickstart Composer File');
 
-    $this->addRepository('drupal', 'composer', 'https://packages.drupal.org/8');
-    $this->addRepository(
-      'commerce_base',
-      'vcs',
-      'https://github.com/drupalcommerce/commerce_base'
-    );
     $this->addRepository(
       'acquia_lightning',
       'vcs',
       'https://github.com/acquia/lightning-project'
     );
 
-    $this->addRequire('ext-curl');
-    $this->addRequire('composer/installers', '^1.2');
-    $this->addRequire('drupal-composer/drupal-scaffold', '^2.2');
-    $this->addRequire('cweagans/composer-patches', '~1.0');
-    $this->addRequire('drupal/core');
-    $this->addRequire('drupal/commerce');
-    $this->addRequire('drupal/swiftmailer');
+    $this->addRequire('acquia/lightning');
+    // todo shawn: do we need these??
+//    $this->addRequire('ext-curl');
+//    $this->addRequire('composer/installers', '^1.2');
+//    $this->addRequire('drupal/swiftmailer');
+    $this->addRequire('drupal/drupal-extension', '~3.2.0');
+    $this->addRequire('behat/mink', '~1.7');
+    $this->addRequire('behat/mink-goutte-driver', '~1.2');
+    $this->addRequire('jcalderonzumba/gastonjs', '~1.0.2');
+    $this->addRequire('drupal/coder', '8.*');
+    $this->addRequire('mikey179/vfsStream', '~1.2');
+    $this->addRequire('phpunit/phpunit', '~4.8');
+    $this->addRequire('symfony/css-selector', '~2.8');
+    $this->addRequire('behat/behat', '^3.0');
+    $this->addRequire('se/selenium-server-standalone', '^2.53');
+    $this->addRequire('composer/composer', '^1.3');
+    $this->addRequire('drush/drush', '8.1.11');
+    $this->addRequire('drupal/console', '^1.0');
+    $this->addRequire('drupal-composer/drupal-scaffold', '^2.0.0');
+    $this->addRequire('cweagans/composer-patches', '^1.6.0');
     $this->addRequire('drupalcommerce/commerce_base');
-
-    $this->addAutoload('classmap', ['scripts/composer/ScriptHandler.php']);
-
-    $this->addScript(
-      'drupal-scaffold',
-      'DrupalComposer\\DrupalScaffold\\Plugin::scaffold'
-    );
-    $this->addScript(
-      'post-update-cmd',
-      ['DrupalProject\\composer\\ScriptHandler::createRequiredFiles']
-    );
   }
 }
