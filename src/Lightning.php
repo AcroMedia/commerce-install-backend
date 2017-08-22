ad8db0523a8e7ed160dcd1b676ba7f4e722d976e
Jace Bennest <jacebennest87@gmail.com>
2017-08-21 22:12:30 -0700

    lightning base finished

--- src/Lightning.php
+++ src/Lightning.php
@@ -7,32 +7,71 @@ class Lightning extends Base {
   public function __construct() {
     parent::__construct('Kickstart', 'Automatically Generated Commerce Kickstart Composer File');
 
+    // Type
+    $this->setType('project');
+
+    // Require Dev
+    $this->addRequireDev('drupal/drupal-extension', '~3.2.0');
+    $this->addRequireDev('behat/mink', '~1.7');
+    $this->addRequireDev('behat/mink-goutte-driver', '~1.2');
+    $this->addRequireDev('jcalderonzumba/gastonjs', '~1.0.2');
+    $this->addRequireDev('drupal/coder', '8.*');
+    $this->addRequireDev('mikey179/vfsStream', '~1.2');
+    $this->addRequireDev('phpunit/phpunit', '~4.8');
+    $this->addRequireDev('symfony/css-selector', '~2.8');
+    $this->addRequireDev('behat/behat', '^3.0');
+    $this->addRequireDev('se/selenium-server-standalone', '^2.53');
+    $this->addRequireDev('composer/composer', '^1.3');
+    $this->addRequireDev('drush/drush', '8.1.11');
+    $this->addRequireDev('drupal/console', '^1.0');
+
+    // Require
+    $this->addRequire('drupal-composer/drupal-scaffold', '^2.0.0');
+    $this->addRequire('cweagans/composer-patches', '^1.6.0');
+    $this->addRequire('acquia/lightning', '~2.1.6');
+
+    // Repositories
     $this->addRepository(
       'acquia_lightning',
       'vcs',
       'https://github.com/acquia/lightning-project'
     );
 
-    $this->addRequire('acquia/lightning');
-    // todo shawn: do we need these??
-//    $this->addRequire('ext-curl');
-//    $this->addRequire('composer/installers', '^1.2');
-//    $this->addRequire('drupal/swiftmailer');
-    $this->addRequire('drupal/drupal-extension', '~3.2.0');
-    $this->addRequire('behat/mink', '~1.7');
-    $this->addRequire('behat/mink-goutte-driver', '~1.2');
-    $this->addRequire('jcalderonzumba/gastonjs', '~1.0.2');
-    $this->addRequire('drupal/coder', '8.*');
-    $this->addRequire('mikey179/vfsStream', '~1.2');
-    $this->addRequire('phpunit/phpunit', '~4.8');
-    $this->addRequire('symfony/css-selector', '~2.8');
-    $this->addRequire('behat/behat', '^3.0');
-    $this->addRequire('se/selenium-server-standalone', '^2.53');
-    $this->addRequire('composer/composer', '^1.3');
-    $this->addRequire('drush/drush', '8.1.11');
-    $this->addRequire('drupal/console', '^1.0');
-    $this->addRequire('drupal-composer/drupal-scaffold', '^2.0.0');
-    $this->addRequire('cweagans/composer-patches', '^1.6.0');
-    $this->addRequire('drupalcommerce/commerce_base');
+    // Scripts
+    $this->addScript(
+      'post-install-cmd',
+      'DrupalComposer\\DrupalScaffold\\Plugin::scaffold'
+    );
+
+    // Config
+    $this->setConfig(['bin-dir' => 'bin/']);
+
+    // Extras
+    $this->setExtra([
+      'installer-types' => ['bower-asset', 'npm-asset'],
+      'installer-paths' => [
+        'docroot/core' => [
+          'type=>drupal-core',
+        ],
+        'docroot/libraries/{$name}' => [
+          'type:drupal-library',
+          'type:bower-asset',
+          'type:npm-asset',
+        ],
+        'docroot/modules/contrib/{$name}' => [
+          'type:drupal-module',
+        ],
+        'docroot/profiles/contrib/{$name}' => [
+          'type:drupal-profile',
+        ],
+        'docroot/themes/contrib/{$name}' => [
+          'type:drupal-theme',
+        ],
+        'drush/contrib/{$name}' => [
+          'type:drupal-drush',
+        ],
+      ],
+      'enable-patching' => TRUE,
+    ]);
   }
 }
